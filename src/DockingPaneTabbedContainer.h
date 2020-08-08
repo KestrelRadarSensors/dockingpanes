35246ed35c358cb5c9fcfb3d54407c51f1e80500
Leandro Soria <leandrosoria@hotmail.com>
2020-08-08 18:17:33 -0300

    Unify public/protected/private sections

--- src/DockingPaneTabbedContainer.h
+++ src/DockingPaneTabbedContainer.h
@@ -35,28 +35,21 @@ class DockingPaneTabbedContainer : public DockingPaneContainer
         Q_OBJECT
 
     public:
+        friend class DockingPaneManager;
         explicit DockingPaneTabbedContainer(QWidget *parent = nullptr);
         ~DockingPaneTabbedContainer();
 
         bool addPane(DockingPaneContainer *child);
-
-        friend class DockingPaneManager;
-
         virtual void saveLayout(QDomNode *parentNode, bool includeGeometry=false);
-
         void restoreChildWidgets(void);
         virtual int getPaneCount(void);
-
         virtual DockingPaneContainer *getPane(int index);
         virtual DockingPaneFlyoutWidget *openFlyout(bool hasFocus, QWidget *parent, FlyoutPosition pos, DockingPaneContainer *pane);
         virtual void setClientWidget(QWidget *widget);
         void setVisiblePane(DockingPaneContainer *pane);
         bool containsPane(DockingPaneContainer *pane);
-
-    private:
-        void calculateButtonsRectangles(void);
-        QRect getButtonRect(int pos);
-        void updateMargins(void);
+        void onCloseButtonClicked(void);
+        void onPinButtonClicked(void);
 
     protected:
         virtual void paintEvent(QPaintEvent* event);
@@ -64,28 +57,13 @@ class DockingPaneTabbedContainer : public DockingPaneContainer
         virtual void mouseReleaseEvent(QMouseEvent *e);
         virtual void mouseMoveEvent(QMouseEvent *e);
         virtual void resizeEvent(QResizeEvent * event);
-
-    signals:
-        
-    protected slots:
         void onStartDragTitle(QPoint pos);
         void onEndDragTitle(QPoint pos);
         void onMoveDragTitle(QPoint pos);
-
         void onStartDragFlyoutTitle(QPoint pos);
         void onEndDragFlyoutTitle(QPoint pos);
         void onMoveDragFlyoutTitle(QPoint pos);
 
-    private slots:
-        void onUnpinContainer(void);
-        void onCloseContainer(void);
-        void onFocusChanged(QWidget *old, QWidget *now);
-
-    public slots:
-        void onCloseButtonClicked(void);
-        void onPinButtonClicked(void);
-
-    protected:
         QStackedWidget *m_stackedWidget;
         QList<DockingPaneContainer *> m_paneList;
         QList<int> m_tabWidths;
@@ -94,7 +72,15 @@ class DockingPaneTabbedContainer : public DockingPaneContainer
         QPoint m_originalClickPos;
         QRect m_invalidTabRect;
         DockingPaneFlyoutWidget *m_flyoutWidget;
-        bool m_fromMousePressEvent;  
+        bool m_fromMousePressEvent;
+
+    private:
+        void calculateButtonsRectangles(void);
+        QRect getButtonRect(int pos);
+        void updateMargins(void);
+        void onUnpinContainer(void);
+        void onCloseContainer(void);
+        void onFocusChanged(QWidget *old, QWidget *now);
 };
 
 #endif // DOCKINGPANETABBEDCONTAINER_H
